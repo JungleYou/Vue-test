ed958ca682e351a7dfc231e7a8da81d8311e7ca7
jungle <2044626051@qq.com>
2022-03-29 20:20:43 +0800

    master

---
+++ 13_src_网易云/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="root">
+    <Search />
+    <Music />
+    <Sum />
+  </div>
+</template>
+
+<script>
+import Search from "./components/Search.vue";
+import Music from "./components/Music.vue";
+import Sum from "./components/Sum.vue";
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  components: {
+    Search,
+    Music,
+    Sum,
+  },
+};
+</script>
+
+<style lang='less'>
+* {
+  margin: 0;
+  padding: 0;
+}
+#root {
+  box-sizing: border-box;
+  width: 600px;
+  border: 2px solid rgb(194, 194, 193);
+  margin: 100px auto;
+  padding: 20px;
+  li {
+    margin: 0;
+    list-style: none;
+  }
+}
+</style>
